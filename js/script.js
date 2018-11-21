250aa5efae12e98d4e59708345805cb67e5ee4e7
Joshua Rodriguez <ewized@gmail.com>
2018-11-20 21:16:00 -0800

    Web App enabled :octocat:

---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(registration => {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope)
+  }).catch(err => err&&console.log('ServiceWorker registration failed: ', err))
+}
---
+++ sw.js
@@ -0,0 +1,3 @@
+'use strict'
+
+// This is where we can do fancy things with service workers but we dont really need any here
