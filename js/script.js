16cc5c99d9c9aef39430d6df197203d10f46b87f
Joshua Rodriguez <ewized@gmail.com>
2018-11-26 07:37:14 -0800

    Add loading spinner

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(registration => {
     console.log('ServiceWorker registration successful with scope: ', registration.scope)
-  }).catch(err => err&&console.log('ServiceWorker registration failed: ', err))
+  }).catch(err => err && console.log('ServiceWorker registration failed: ', err))
 }
